a72a59d26050975782c83b03dce6e7b954ca81f0
Jasmeetsingghh <120304081+Jasmeetsingghh@users.noreply.github.com>
2024-03-02 18:33:55 +0530

    Time: 818 ms (94.93%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,6 +1,2 @@
 /* Write your PL/SQL query statement below */
--- select firstName,lastName, city ,state from person p left join Address a on p.personId=a.personId where p.personId !=a.personId;
-
-SELECT p.firstName, p.lastName, a.city, a.state
-FROM person p
-LEFT JOIN Address a ON p.personId = a.personId;
+select p.firstName, p.lastName, a.city, a.state from Person p left join Address a on p.personId=a.personId ;
