da391db50564b3d623a3913bda6817b3348b4780
Jasmeetsingghh <120304081+Jasmeetsingghh@users.noreply.github.com>
2024-07-02 15:52:49 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (63.53%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,33 +1,33 @@
-// class Solution {
-//     public String largestOddNumber(String num) {
-//         int n=num.length();
-//         for(int i=n-1;i>=0;i--)
-//         {
-//             // System.out.println((int)num.charAt(i)%2);
-//             if((int)num.charAt(i)%2==1)
-//             {
-//                 return num.substring(0,i+1);
-//             }
-//         }
-//         return new String("");
-//     }
-// }
-
 class Solution {
     public String largestOddNumber(String num) {
-        int maxOddIndex = -1;
-
-        for (int i = 0; i < num.length(); i++) {
-            char currentChar = num.charAt(i);
-            if ((currentChar - '0') % 2 != 0) {
-                maxOddIndex = i;
+        int n=num.length();
+        for(int i=n-1;i>=0;i--)
+        {
+            // System.out.println((int)num.charAt(i)%2);
+            if(num.charAt(i)%2==1)
+            {
+                return num.substring(0,i+1);
             }
         }
-
-        if (maxOddIndex == -1) {
-            return ""; // No odd number found
-        } else {
-            return num.substring(0, maxOddIndex + 1);
-        }
+        return new String("");
     }
 }
+
+// class Solution {
+//     public String largestOddNumber(String num) {
+//         int maxOddIndex = -1;
+
+//         for (int i = 0; i < num.length(); i++) {
+//             char currentChar = num.charAt(i);
+//             if ((currentChar - '0') % 2 != 0) {
+//                 maxOddIndex = i;
+//             }
+//         }
+
+//         if (maxOddIndex == -1) {
+//             return ""; // No odd number found
+//         } else {
+//             return num.substring(0, maxOddIndex + 1);
+//         }
+//     }
+// }
